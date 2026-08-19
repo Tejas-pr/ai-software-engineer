c2392507cc0559f4cd5f983cee62abbbc85c2689
Tejas-pr <Tejas.teju02@gmail.com>
2026-08-19 23:28:54 +0530

    feat: initialize python backend and add shared button component

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Button as ButtonPrimitive } from "@base-ui/react/button"
 import { cva, type VariantProps } from "class-variance-authority"
 
@@ -54,3 +55,4 @@ function Button({
 }
 
 export { Button, buttonVariants }
+
